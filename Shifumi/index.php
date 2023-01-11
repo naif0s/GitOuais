b936e27ff2272955314a3fb77b77e241473e4a53
naif0s <rawzed12@gmail.com>
2023-01-11 11:06:42 +0100

    Ajout du projet Shifumi

---
+++ Shifumi/index.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<html lang="fr">
+<form action="" method="POST">
+    <p>Choisissez votre option :</p>
+    <input type="radio" name="choix" value="Pierre">Pierre<br>
+    <input type="radio" name="choix" value="Feuille">Feuille<br>
+    <input type="radio" name="choix" value="Ciseaux">Ciseaux<br><br>
+    <input type="submit" value="Envoyer">
+</form>
+</html>
